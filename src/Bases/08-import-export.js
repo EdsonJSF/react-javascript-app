3c1f19db730534c1141b84dbcc5a9006e883c888
Edsonjsf <edsonjsf1990@gmail.com>
2023-02-03 14:03:01 -0400

    Múltiples exportaciones y exportaciones por defecto

---
+++ src/Bases/08-import-export.js
@@ -0,0 +1,18 @@
+import defaultExport, { owners } from "../data/heroes";
+
+console.log(owners);
+console.log(defaultExport);
+
+// Metodo find
+const getHeroeById = (id) => {
+  return defaultExport.find((heroe) => heroe.id === id);
+};
+console.log(getHeroeById(2));
+
+// Metodo filter
+const getHeroeByOwner = (owner) => {
+  return defaultExport.filter((heroe) =>
+    heroe.owner.toLocaleLowerCase().includes(owner.toLocaleLowerCase())
+  );
+};
+console.log(getHeroeByOwner("DC"));
