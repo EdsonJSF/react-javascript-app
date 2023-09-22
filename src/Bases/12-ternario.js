d104da740d57d73e058eb7899cfdcc4986bdb466
EdsonJSF <edsonjsf1990@gmail.com>
2023-09-22 14:01:38 -0400

    Ternarios

---
+++ src/Bases/12-ternario.js
@@ -0,0 +1,14 @@
+const active = true;
+
+// let msg = "";
+// if (active) {
+//   msg = "Activo";
+// } else {
+//   msg = "Inactivo";
+// }
+
+// const msg = active ? "Activo" : "Inactivo";
+// const msg = active ? "Activo" : null;
+const msg = active && "Activo";
+
+console.log(msg);
